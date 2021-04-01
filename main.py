8b31475e4e0e301c5eb217ec83dec43eef4ba2b1
Abdul Kholiq <kholiqcode@gmail.com>
2021-04-01 15:34:51 +0700

    Feature Selection

--- main.py
+++ main.py
@@ -7,13 +7,12 @@
 # 7 Remove Stopword
 # 8 Pembobotan Kata (TF/IDF)
 
-import re
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+import core
+import json
 
 # static data
 g1 = "saaaarapan roti biar kamu kaya bule"
-g2 = "pelit kayaaaaa Orangggg cina anjing"
+g2 = "pelit kayaaaaa saaaarapan Orangggg cina anjing"
 g3 = "dia pantes lelet kamu Kaya orang jawa"
 g4 = "negro"
 g5 = "kamu orang timur biasanya yg rusuh"
@@ -22,41 +21,25 @@ g7 = "semoga keturunan A'r*ab muka teroris"
 tweets = [g1, g2, g3, g4, g5, g6, g7]
 
 # Cleaning
-tweets_cleaning = [re.sub(r"[()\"#/@;:<>{}*`'+=~|.!?,]", "", tweet)
-                   for tweet in tweets]
+tweets_cleaning = core.Cleaning(tweets)
 
 # Casefolding
-tweets_casefolding = [tweet_cleaning.lower()
-                      for tweet_cleaning in tweets_cleaning]
+tweets_casefolding = core.Casefolding(tweets_cleaning)
 
 # Tokenizing
-tweets_tokenizing = []
-for tweet_casefolding in tweets_casefolding:
-    tweets_tokenizing.append(tweet_casefolding.split())
+tweets_tokenizing = core.Tokenizing(tweets_casefolding)
 
 # Normalisasi
-tweets_normalization = []
-pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
-for tweet_tokenizing in tweets_tokenizing:
-    tweet_normalization = [pattern.sub(r"\1", tweet_per_word)
-                           for tweet_per_word in tweet_tokenizing]
-    tweets_normalization.append(tweet_normalization)
+tweets_normalization = core.Normalization(tweets_tokenizing)
 
 # Steeming
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
-tweets_steeming = [stemmer.stem(" ".join(tweet_normalization))
-                   for tweet_normalization in tweets_normalization]
+tweets_steeming = core.Steeming(tweets_normalization)
 
 # Remove Stopword
-factory = StopWordRemoverFactory()
-stopword = factory.create_stop_word_remover()
-tweets_stopword = [stopword.remove(tweet_steeming)
-                   for tweet_steeming in tweets_steeming]
+tweets_stopword = core.Stopword(tweets_steeming)
 
 # Pembobotan Kata
-tweets_stopword = [tweet_stopword.split()
-                   for tweet_stopword in tweets_stopword]
+tweets_stopword = core.Pembobotan(tweets_stopword)
 
 
 twets = []
@@ -68,13 +51,13 @@ for tweet in twets:
     _DF = 0
     for index, tweet_stopword in enumerate(tweets_stopword):
         if tweet in tweet_stopword and tweet in _TF_dict:
-            _TF_dict["Data"+str(index)] += 1
+            _TF_dict["Tweet"+str(index)] += 1
             _DF += 1
         elif tweet in tweet_stopword and tweet not in _TF_dict:
-            _TF_dict["Data"+str(index)] = 1
-            _DF = 1
+            _TF_dict["Tweet"+str(index)] = 1
+            _DF += 1
         else:
-            _TF_dict["Data"+str(index)] = 0
+            _TF_dict["Tweet"+str(index)] = 0
     _TF_dict["DF"] = _DF
     _TF_dict["IDF"] = _DF/len(tweets_stopword)
     TWEETS[tweet] = _TF_dict
@@ -84,34 +67,19 @@ RESULT_TF_IDF = {}
 for tweet in twets:
     _TF_IDF_dict = {}
     for index, tweet_stopword in enumerate(tweets_stopword):
-        _TF_IDF_dict["Data"+str(index)] = TWEETS[tweet]["Data" +
-                                                        str(index)]*TWEETS[tweet]["IDF"]
+        _TF_IDF_dict["Tweet"+str(index)] = TWEETS[tweet]["Tweet" +
+                                                         str(index)]*TWEETS[tweet]["IDF"]
     RESULT_TF_IDF[tweet] = _TF_IDF_dict
 
-print(RESULT_TF_IDF)
+# print(RESULT_TF_IDF)
+# print(json.dumps(TWEETS.keys()))
+# key_list = list(TWEETS.keys())
+# print(key_list[0])
 
+# Analisa Fitur Selection
+_FS = {}
+for index, TWEET in enumerate(TWEETS):
+    _FS[TWEET] = RESULT_TF_IDF[TWEET]
+    _FS[TWEET]['DF'] = TWEETS[TWEET]['DF']
 
-# TF_dict = {}
-# DF_dict = {}
-# for index, tweet_stopword in enumerate(tweets_stopword):
-#     WD_dict = {}
-#     DF = 0
-#     for word_twet in twets:
-#         if word_twet in WD_dict and word_twet in tweet_stopword:
-#             WD_dict[word_twet] += 1
-#             DF += 1
-#         elif word_twet not in WD_dict and word_twet in tweet_stopword:
-#             WD_dict[word_twet] = 1
-#             DF += 1
-#         else:
-#             WD_dict[word_twet] = 0
-#     for word_tweet in tweet_stopword:
-#         if word_tweet in DF_dict:
-#             DF_dict[word_tweet] += 1
-#         else:
-#             DF_dict[word_tweet] = 1
-#     TF_dict[index] = WD_dict
-# TF_dict["DF"] = DF_dict
-# TF_dict["IDF"] = [DF_dict[twet]/len(tweets_stopword)
-#                   for twet in twets]
-# print(TWEET)
+print(_FS)
